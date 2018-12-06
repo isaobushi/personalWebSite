e83cf46adde88c9c4546c11ddcc78c462192a0b9
Andrea Mele <andrea.mele.au@gmail.com>
2018-12-07 09:08:56 +1100

    skill part finished

--- web.js
+++ web.js
@@ -1,84 +1,26 @@
 
 
-const icon = document.querySelector('.sphere')
 const greetings = document.querySelector('.greetings')
 const containerCenter = document.querySelector('.initial-center')
+const containerHeader = document.querySelector('.initial-header')
 const containerPage = document.querySelectorAll('.container-page')
-const about = document.querySelector('.about')
-const skills = document.querySelector('.skills')
-const portfolio = document.querySelector('.portfolio')
-const contacts = document.querySelector('.contacts')
 const vH = document.documentElement.clientHeight
 const vW = document.documentElement.clientWidth
+const subIntro = document.querySelector('.sub-intro')
+const iconsBox = document.querySelector('.containerIcons')
+const btnSkills = document.querySelector('.btn-skills')
+const textAbout = document.querySelector('.text-about')
+const aElem = document.querySelector('.btn-hero')
 
 
 
-
-let aboutPhrase = `Hello, I'm Andrea Mele. 
-I'm a full-stack web developer. 
-Fast load times and lag free interaction, my highest priority. 
-My layouts will work on any device, big or small. 
-Strong preference for easy to use, intuitive UX/UI. 
-Websites don't have to be static, I love making pages come to life.`
-let arraySplitText = aboutPhrase.split('')
-let letterNodes = document.createElement('span')
-arraySplitText.forEach(character => {
-  let charElement = document.createElement('span')
-  charElement.textContent = `${character}`
-  charElement.classList.add('letters')
-  charElement.style.whiteSpace = 'pre'
-  charElement.style.position = 'relative'
-  charElement.style.fontSize = '3rem'
-  charElement.style.opacity = '0'
-  charElement.style.display = 'inline-block'
-  // charElement.style.transitionDelay = `${delay}ms`
-  charElement.style.transition = 'all .3s linear'
-  letterNodes.appendChild(charElement)
-  // character.style.transition = `all, .5s linear`
-})
-about.appendChild(letterNodes)
-const letters = document.querySelectorAll('.letters')
-
-const splat = (letter, index) => {
-  const randX = Math.round(Math.random() * vW)
-  const randY = Math.round(Math.random() * vH)
-  const randSide = Math.round(Math.random() * 1) === 0 ? '-' : '+'
-  letter.style.fontSize = `${Math.round(Math.random() * 40)}px`
-  letter.style.transform = `translate(${randSide + randX}px, ${randSide + randY}px)`
-  letter.style.transitionDelay = `${index * 10}ms`
-}
-
-
-const divide = () => {
-  for (let index = 0; index < letters.length; index++) {
-    splat(letters[index], index)
-  }
-}
-
-const stitch = () => {
-  letters.forEach(letter => {
-    letter.style.fontSize = `25px`
-    letter.style.opacity = `1`
-    letter.style.transform = `translate(0, 0)`
-    letter.style.color = 'white'
-  })
-}
-
-
-const goBackTogheter = () => {
-  for (let index = 0; index < letters.length; index++) {
-    stitch(letters[index])
-  }
-
-}
 
 let opened = false
 const changeGreetings = () => {
   if (opened === false) {
     greetings.style.opacity = '1'
     greetings.style.opacity = '0'
-    // greetings.textContent = 'Welcome'
-    opened = true
+     opened = true
     setTimeout(() => requestAnimationFrame(changeToWelcome), 1000)
   }
   else {
@@ -87,89 +29,198 @@ const changeGreetings = () => {
 }
 
 const changeToWelcome = () => {
-  greetings.textContent = 'Welcome'
-  greetings.style.opacity = '1'
+  greetings.innerHTML = '<h1 class=welcome>Welcome</h1>'
   setTimeout(() => requestAnimationFrame(squash), 1000)
-  divide()
 }
 
 const squash = () => {
-  icon.style.opacity = '0'
   greetings.style.transition = 'all, .5s ease-in-out'
   greetings.style.opacity = '0'
   containerCenter.style.height = '0'
-  containerCenter.style.border = '1px solid black'
+  containerCenter.style.border = '1px solid rgb(75, 75, 75)'
+
   setTimeout(() => requestAnimationFrame(eliminateIntroduction), 1000)
 }
 
 const eliminateIntroduction = () => {
-  icon.style.display = 'none'
   greetings.style.display = 'none'
-  containerPage.forEach(page => {
-    page.style.width = '40vh'
-    page.style.display = 'flex'
-  })
-  setTimeout(() => openMain(), 300)
+  subIntro.style.display = 'none'
+  subIntro.style.display = 'none'
+  aElem.style.display = 'none'
+ 
+  requestAnimationFrame(openMain)
 }
 
 const openMain = () => {
-  containerCenter.style.border = '.3px solid black'
+  containerCenter.style.border = '.3px solid #cacaca'
   containerCenter.style.height = '100vh'
-  containerPage.forEach(page => {
-    page.style.height = '80vh'
-    page.style.opacity = '1'
-    page.style.boxShadow = '5px 5px 5px grey'
+  // iconsBox.style.transform = 'translateY(50rem)'
+  let containerAbout = document.createElement('div')
+  let textAbout = document.createElement('div')
+  containerAbout.classList.add('.container-about')
+  textAbout.innerHTML =`
+  I'm a full-stack web developer.
+  Fast load times and lag free <span class='yellow'>interaction</span>, 
+  my highest priority. 
+  My layouts will work on any device, big or small. 
+  Strong preference for easy to use, <span class='yellow'>intuitive</span> UX/UI. 
+  Websites don't have to be static, 
+  I love making pages come to <span class='yellow'>Life</span>.
+  <span class='yellow'> \\ (º – º) /</span>`
+  textAbout.classList.add('text-about')
+  containerAbout.appendChild(textAbout)
+  containerCenter.appendChild(containerAbout)
+  containerCenter.insertBefore(containerAbout, containerCenter.firstChild);
+}
+
+
+const closeFirstPage = () => {
+  document.querySelector('.initial-center').style.height = '0'
+  setTimeout(() => requestAnimationFrame(close), 1000)
+}
+const close = () => {
+  const destroy = () => {
+    skills.remove()
+    setTimeout(() => requestAnimationFrame(openPortfolio), 1000)
+    
+  }
+  const closeSkills = () => {
+    skills.style.height = '0vh'
+    setTimeout(()=> requestAnimationFrame(destroy), 1000)
+  }
+
+  document.querySelector('.initial-center').lastChild.remove()
+  // document.querySelector('.initial-center').style.height = '100vh'
+  let skills = document.createElement('div')
+  skills.classList = 'skills'
+  document.querySelector('.initial-center').appendChild(skills)
+  skills.style.opacity = '1'
+  skills.innerHTML = `
+  <div class=box-skills> 
+    <div class=text-skills> C/> Downloading Skills package
+      <span class=dots>...</span> 
+      <span class=cursor skills>&#128126;</span>
+    </div>
+    <div class="bar-out">
+      <div class="bar-in"> </div> 
+    </div> 
+  </div>`
+  
+  let skillsIcons = document.createElement('div')
+  const reactIcons = document.createElement('div')
+  const htmlIcons = document.createElement('div')
+  const jsIcons = document.createElement('div')
+  const rubyIcons = document.createElement('div')
+  const nodeJS = document.createElement('div')
+  const postgreSqlIcons = document.createElement('div')
+  const sassIcons = document.createElement('div')
+
+  const cssIcons = document.createElement('div')
+  const gitHubIcons = document.createElement('div')
+  const rubyOnRailsIcons = document.createElement('div')
+
+
+  const arrSkills = [reactIcons, htmlIcons, jsIcons, rubyIcons, nodeJS, postgreSqlIcons,cssIcons,rubyOnRailsIcons,gitHubIcons, sassIcons]
+  reactIcons.style.backgroundImage = `url(/images/React-icon.svg)`
+  gitHubIcons.style.backgroundImage = `url(/images/Octicons-mark-github.svg)`
+  htmlIcons.style.backgroundImage = `url(/images/HTML5_logo_and_wordmark.svg)`
+  cssIcons.style.backgroundImage = `url(/images/CSS.3.svg)`
+  jsIcons.style.backgroundImage = `url(/images/Unofficial_JavaScript_logo_2.svg)`
+  rubyIcons.style.backgroundImage = `url(/images/Ruby_logo.svg)`
+  rubyOnRailsIcons.style.backgroundImage = `url(/images/Ruby_On_Rails_logo.svg)`
+  nodeJS.style.backgroundImage = `url(/images/node.svg)`
+  postgreSqlIcons.style.backgroundImage = `url(/images/psql.png)`
+  sassIcons.style.backgroundImage = `url(/images/psql.png)`
+
+  
+  arrSkills.forEach(skill => {
+    skill.classList = 'skill-icon'
+    skill.style.backgroundColor = 'transparent'
+    skill.style.backgroundSize = 'contain'
+    skill.style.backgroundRepeat = 'no-repeat'
+    skill.style.height = '5rem'
+    skill.style.width = '5rem'
+    skill.style.borderRadius = '.5rem'
+    skill.style.transition = 'all 1s linear'
   })
+  skillsIcons.style.padding = ' '
+  skillsIcons.classList = 'skills-icons-box'
+  skillsIcons.style.display = 'grid'
+  skillsIcons.style.gridTemplateColumns = '5rem 5rem 5rem 5rem 5rem'
+  skillsIcons.style.gridTemplateRows = '5rem 5rem' 
+  skillsIcons.style.gridGap = '3rem 7rem'
+  skillsIcons.style.width = '50vw'
+  skillsIcons.style.margin= '2%'
+  skillsIcons.style.height = '20vh'
+  skills.appendChild(skillsIcons)
+  let elem = document.querySelector('.bar-in')
+  const barGrow = () => {
+    let barWidth = 0;
+    let id = setInterval(frame, 35)
+    function frame() {
+      if (barWidth >= 40) {
+        elem.textContent = 'COMPLETED!'
+        let containerBtnPortfolio = document.createElement('div')
+        let buttonPortfolio = document.createElement('button')
+        buttonPortfolio.classList = 'btn-portfolio'
+        containerBtnPortfolio.classList = 'box-btn-portfolio'
+
+        buttonPortfolio.textContent = 'Portfolio'
+        buttonPortfolio.addEventListener('click', closeSkills)
+        containerBtnPortfolio.appendChild(buttonPortfolio)
+        skills.appendChild(containerBtnPortfolio)
+        clearInterval(id);
+      } else {
+        barWidth++;
+        if (barWidth === 5 || barWidth === 10 || barWidth === 15 || barWidth === 20 || barWidth === 25 || barWidth === 28|| barWidth === 30 || barWidth === 34  || barWidth === 35  || barWidth === 40)
+        {skillsIcons.appendChild(arrSkills.shift())}
+        // elem.style.backgroundColor = '#16a085'
+        elem.style.width = `${barWidth}vw`
+      }
+    }
+  }
+  barGrow()
 }
 
 
-const openAbout = () => {
-  skills.textContent = ''
-  portfolio.textContent = ''
-  contacts.textContent = ''
-  skills.style.width = '0'
-  portfolio.style.width = '0'
-  contacts.style.width = '0'
-  skills.style.height = '0'
-  portfolio.style.height = '0'
-  contacts.style.height = '0'
-  skills.style.transform = 'rotate(90deg)'
-  portfolio.style.transform = 'rotate(90deg)'
-  contacts.style.transform = 'rotate(90deg)'
-  skills.style.position = 'fixed'
-  portfolio.style.position = 'fixed'
-  contacts.style.position = 'fixed'
-  skills.style.transform = 'translate(48vw,-30vh)'
-  portfolio.style.transform = 'translate(48vw,-35vh)'
-  contacts.style.transform = 'translate(48vw,-40vh)'
-
-  // skills.style.position = 'fixed'
-  // portfolio.style.position = 'fixed'
-  // contacts.style.position = 'fixed'
-  about.style.marginRight = '3rem'
-  about.style.width = '100vw'
-  about.style.width = '100vw'
-  // about.style.padding = '30rem'
-  requestAnimationFrame(throwLettersIn)
-}
-const throwLettersIn = () => {
-  // about.style.transform = 'scale(1.)'
-  // about.style.width = '100vw'
-  letters.forEach(letter => {
-    letter.style.opacity = '1'
-  })
-
-  requestAnimationFrame(goBackTogheter)
+const openPortfolio = () => {
+  document.querySelector('.initial-center').remove()
 
 }
 
-// const killPages = () => {
-//   skills.style.display = 'none'
-//   portfolio.style.display = 'none'
-//   contacts.style.display = 'none'
-//   goBackTogheter()
 
-// }
+const docStyle = document.documentElement.style
+const boundingClientRect = aElem.getBoundingClientRect()
 
-window.addEventListener('click', changeGreetings)
-about.addEventListener('click', openAbout)
+aElem.onmousemove = function(e) {
+
+	const x = e.clientX - boundingClientRect.left
+	const y = e.clientY - boundingClientRect.top
+	
+	const xc = boundingClientRect.width/2
+	const yc = boundingClientRect.height/2
+	
+	const dx = x - xc
+	const dy = y - yc
+	
+	docStyle.setProperty('--rx', `${ dy/-1 }deg`)
+	docStyle.setProperty('--ry', `${ dx/10 }deg`)
+	
+}
+
+aElem.onmouseleave = function(e) {	
+	docStyle.setProperty('--ty', '0')
+	docStyle.setProperty('--rx', '0')
+	docStyle.setProperty('--ry', '0')	
+}
+
+aElem.onmousedown = function(e) {
+	docStyle.setProperty('--tz', '-25px')
+}
+document.body.onmouseup = function(e) {
+	
+	docStyle.setProperty('--tz', '-12px')
+}
+
+aElem.addEventListener('click', changeGreetings)
+btnSkills.addEventListener('click', closeFirstPage)
